11fc3ac6f6d4f89be836cce10e82f168c9fc5c09
Mario <you@example.com>
2018-07-01 17:55:07 -0600

    Tocando js

--- Examen/client/main.js
+++ Examen/client/main.js
@@ -1,22 +1,60 @@
 import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
+import { Messages } from '../lib/collections.js';
+import { Accounts } from 'meteor/accounts-base';
+
+// Accounts config
+Accounts.ui.config({
+  passwordSignupFields:'USERNAME_ONLY'
+});
 
 import './main.html';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
+Template.body.helpers({
+  /*
+  messages:[
+    {text:'My Note 1'},
+    {text:'My Note 2'},
+    {text:'My Note 3'}
+  ]
+  */
+  messages(){
+    return Messages.find({});
+  }
 });
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
+Template.add.events({
+  'submit .add-form': function(){
+    event.preventDefault();
+
+    // Get input value
+    const target = event.target;
+    const text = target.text.value;
+
+    // Insert note into collection
+    /*
+    Messages.insert({
+      text,
+      ceratedAt: new Date(),
+      owner: Meteor.userId(),
+      username: Meteor.user().username,
+    });
+    */
+    Meteor.call('messages.insert', text);
+
+    // Clear form
+    target.text.value = '';
+
+    // Close modal
+    $('#addModal').modal('close');
+
+    return false;
+  }
 });
 
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
+Template.note.events({
+  'click .delete-note':function(){
+    //Messages.remove(this._id);
+    Meteor.call('messages.remove', this);
+    return false;
+  }
 });
