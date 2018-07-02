c207cc7efac0c3c64d6f9205e40504470d12a301
Mario <you@example.com>
2018-07-01 18:53:01 -0600

    Conectando con Mongo

---
+++ Examen/lib/collections.js
@@ -0,0 +1,32 @@
+import { Mongo } from 'meteor/mongo';
+import { Meteor } from 'meteor/meteor';
+import { check } from 'meteor/check';
+
+export const Messages = new Mongo.Collection('messages');
+
+Meteor.methods({
+  'messages.insert'(text){
+    check(text, String);
+
+    // Check if user is logged in
+    if(!Meteor.userId()){
+      throw new Meteor.Error('not-authorized');
+    }
+    var counter = Meteor.user().counter;
+    Messages.insert({
+      text,
+      ceratedAt: new Date(),
+      owner: Meteor.userId(),
+      username: Meteor.user().username,
+    });
+  },
+  'Messages.remove'(note){
+    check(note._id, String);
+
+    if(note.owner !== Meteor.userId()){
+      throw new Meteor.Error('not-authorized');
+    }
+
+    Messages.remove(note._id);
+  }
+});
