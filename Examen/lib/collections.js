8a06bd0cdb152a9c88bfef429ff09e37147917cd
Mario <you@example.com>
2018-07-01 23:03:02 -0600

    Terminando

--- Examen/lib/collections.js
+++ Examen/lib/collections.js
@@ -5,25 +5,26 @@ import { check } from 'meteor/check';
 export const Messages = new Mongo.Collection('messages');
 
 Meteor.methods({
-  'messages.insert'(text){
+  'messages.insert'(text) {
     check(text, String);
 
     // Check if user is logged in
-    if(!Meteor.userId()){
+    if (!Meteor.userId()) {
       throw new Meteor.Error('not-authorized');
     }
-    var counter = Meteor.user().counter;
+    //var counter = Meteor.user().counter;
     Messages.insert({
       text,
       ceratedAt: new Date(),
       owner: Meteor.userId(),
       username: Meteor.user().username,
+      counter: Meteor.user().counter,
     });
   },
-  'Messages.remove'(note){
+  'Messages.remove'(note) {
     check(note._id, String);
 
-    if(note.owner !== Meteor.userId()){
+    if (note.owner !== Meteor.userId()) {
       throw new Meteor.Error('not-authorized');
     }
 
